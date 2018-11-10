ac3bf9da8f90a0ea60a807d8f254121799721f34
Sagar Ghimire <sagar@growcept.com>
2018-11-10 17:55:25 +1100

    Add New Quote Functionality

--- src/App.js
+++ src/App.js
@@ -5,31 +5,33 @@ import Axios from 'axios';
 class App extends Component {
 
   state = {
-    quotes: []
+    quotes: [],
+    content: "Life isn’t about getting and having, it’s about giving and being.",
+    author: "Kevin Kruse"
   }
 
   getQuotes = () => {
-    Axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json').then(response => {
+    Axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+    .then( response => {
       const quotes = response.data.quotes
       this.setState({
         quotes
-      })
+      }, () => { this.showSingleQuote() })
     })
     .catch( error => {
       console.log(error)
     })
   }
 
+  getRandomQuote = () => {
+    return this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)];
+  }
+
   showSingleQuote = () => {
-    let quotes = this.state.quotes
-    console.log(quotes[0])
-    return quotes.map( quote => {
-      return (
-        <React.Fragment>
-          <div id="text">{quote.quote}</div>
-          <div id="author">{quote.author}</div>
-        </React.Fragment>
-      )
+    let quotes = this.getRandomQuote()
+    this.setState({
+      content: quotes.quote,
+      author: quotes.author
     })
   }
 
@@ -41,8 +43,9 @@ class App extends Component {
     return (
       <div className="App">
         <div id="quote-box">
-          { this.showSingleQuote() }
-          <button id="new-quote">New Quote</button>
+          <div>{ !this.state.content ? 'Loading' : this.state.content }</div>
+          <div>{ !this.state.author ? 'Loading' : this.state.author }</div>
+          <button id="new-quote" onClick={ this.showSingleQuote }>New Quote</button>
           <button id="tweet-quote">Tweet</button>
         </div>
       </div>
